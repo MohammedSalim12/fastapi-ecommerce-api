ec48e06e9e4eb4d8248d9811afeb01d56075f298
Mohammed <mohammed@gmail.com>
2026-02-23 01:59:14 +0300

    add users table creation

--- database/database_connection.py
+++ database/database_connection.py
@@ -8,3 +8,36 @@ import mysql.connector
 #)
 
  # mycursor = dbcon.cursor()
+
+import mysql.connector
+import os
+
+dbcon = mysql.connector.connect(
+    host=os.getenv("mysql.railway.internal"),
+    user=os.getenv("root"),
+    password=os.getenv("OLgxgwLQhjSIMRLkphfycrEbEcbZaZIi"),
+    database=os.getenv("railway"),
+    port=os.getenv("3306")
+)
+
+mycursor = dbcon.cursor(dictionary=True)
+print("DB Connected Successfully")
+
+def create_users_table():
+    try:
+        mycursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(255),
+                email VARCHAR(255) UNIQUE,
+                password VARCHAR(255),
+                phone VARCHAR(20)
+            );
+        """)
+        dbcon.commit()
+        print("Users table created successfully")
+    except Exception as e:
+        print("Error creating table:", e)
+        
+# استدعاء الدالة 
+create_users_table()
